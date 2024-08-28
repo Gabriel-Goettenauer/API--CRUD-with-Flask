12e17a975243bb01b40c564b87e70f17af7a67e0
Gabriel Goettenauer <gabrielgoettenauer9@gmail.com>
2024-08-28 14:40:14 -0300

    Testes automatizados criados com sucesso

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def create_task():
       
     print(tasks) 
     
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
